65fd00e5b8c70ac1fb34ed6ee5a39e4c8ae6db7c
RapipIyaa <mirzaakbarfahrezi@gmail.com>
2025-03-08 06:00:31 +0700

    Add files via upload

---
+++ Pertemuan-2/variable_user.php
@@ -0,0 +1,10 @@
+<?php
+    // Mendefinisikan variables
+    $nama = "Muhammad Messi";
+    $umur = 18;
+    $bb = 60;
+
+    echo "Nama saya adalah : " .$nama;
+    echo "<br/>Umur : " .$umur. " Tahun";
+    echo "<br/>Berat badan : " .$bb. " Kg";
+?>
